2c3d2212359a1ab12c6bfc3bfe186ced49c357e9
Samrath Singh Kalra <samrathsinghrohit@gmail.com>
2023-03-11 02:02:30 +0530

    Structure Changed

--- client/src/Footer.tsx
+++ client/src/Footer.tsx
@@ -4,8 +4,10 @@ import './App.css'
 const Footer: React.FC = () => {
   return (
     <>
-      <div className='bg-black w-full h-auto'>
-        
+      <div className='bg-black w-full h-auto fixed bottom-0'>
+      <a href="https://www.instagram.com/samrathsinghrohit/" target="_blank"><i className="fab fa-instagram" id="insta"></i></a>
+      <a href="https://linkedin.com/in/samrath-singh-kalra/" target="_blank"><i className="fab fa-linkedin" id="linkedin"></i></a>
+      <a href="https://github.com/hitman-samrath" target="_blank"><i className="fab fa-github" id="github"></i></a>
         <p className='text-white'>Created by <span className='text-yellow-400'>Samrath Singh Kalra</span></p>
       </div>
     </>
