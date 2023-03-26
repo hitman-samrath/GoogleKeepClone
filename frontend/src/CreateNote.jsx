12535c712ee80820f619983dd4e8334916114224
Samrath Singh Kalra <samrathsinghrohit@gmail.com>
2023-03-26 22:16:18 +0530

    Hide Feature Added

--- frontend/src/CreateNote.jsx
+++ frontend/src/CreateNote.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import Note from './Note';
 
 const CreateNote = () => {
+
+    const [visibility, setVisibility]=useState(false);
+
     const [note, setNote]=useState({
         heading:"",
         content:""
     });
+
     const [allNotesData, setAllNotesData]=useState([]);
 
     const addNote=(props)=>{
@@ -29,16 +32,24 @@ const CreateNote = () => {
         });
     }
 
-    const checkData=()=>{
+    const checkData=(props)=>{
+        props.preventDefault();
         return note.heading.length===0 || note.content.length===0?alert('Please fill in all the fields'):setData();
+    }
+
+    const showNote=()=>{
+        setVisibility(true);
+    }
+    const hideNote=()=>{
+        setVisibility(false);
     }
 
     return (
         <>
-            <div className='m-auto pt-1 mt-5 w-64 xs:w-80 shadow-lg rounded-xl shadow-slate-400 font-cursive'>
-                <input type="text" name="heading" value={note.heading} placeholder="Add Title" onChange={addNote} className='w-full p-3 m-0 font-bold outline-none required'/>
-                <textarea type="text" name="content" value={note.content} placeholder="Add a note..." rows="6" onChange={addNote} className='pl-3 w-3/4 xs:w-5/6 outline-none resize-none scrollbar-hidden required'/>
-                <button className='ml-2 mr-1 mb-3 w-10 h-10 shadow-sm shadow-slate-700 rounded-full bg-white text-center text-4xl text-yellow-400' onClick={checkData}>+</button>
+            <div onDoubleClick={hideNote} className='m-auto pt-1 mt-5 w-64 xs:w-80 shadow-lg rounded-xl shadow-slate-400 font-cursive'>
+                {visibility?<input type="text" name="heading" value={note.heading} placeholder="Add Title" onChange={addNote} className='w-full p-3 m-0 font-bold outline-none required'/>:<input type="text" placeholder="Add a note" onClick={showNote} className='w-full p-3 m-0 font-bold outline-none rounded-xl'></input>}
+                {visibility?<textarea type="text" name="content" value={note.content} placeholder="Add a note..." rows="6" onChange={addNote} className='pl-3 w-3/4 xs:w-5/6 outline-none resize-none scrollbar-hidden required'/>:null}
+                {visibility?<button className='ml-2 mr-1 mb-3 w-10 h-10 shadow-sm shadow-slate-700 rounded-full bg-white text-center text-4xl text-yellow-400' onClick={checkData}>+</button>:null}
             </div>
             <div className='flex md:m-8 lg:m-12 mt-8 justify-center flex-wrap'>
                 {allNotesData.map((value, index)=>{
--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <header className='bg-yellow-400 text-white font-tnr text-3xl font-bold p-3 md:pl-28 md:pr-28 text-center md:text-left'>
+        <header className='bg-yellow-400 text-white font-tnr text-3xl font-bold p-3 text-center md:pl-28 md:pr-28 md:text-left'>
             Samrath Notes App
         </header>
     )
